82d123b27605c86cf26ced668d7f294b509d8ec3
beatrice <beatrice.pepple@gmail.com>
2022-12-10 09:36:46 +0100

    linked studentinput score to the grades

--- js/gradesheet.js
+++ js/gradesheet.js
@@ -4,3 +4,23 @@ Date: 10th Dec, 2022
 Author: Beatrice Pepple
 */
 
+function gradeChecker(score){
+	var message = ''; //this is a local variable for this function
+	
+	if(score <40){
+		message = 'F'
+	}else if(score >=40 && score <45){
+		message = 'E'
+	}else if(score >=45 && score <50){
+		message = 'D'
+	}else if(score >=50 && score <60){
+		message = 'C'
+	}else if(score >=60 && score <=69){
+		message = 'B'
+	}else if(score >=70 && score <=100){
+		message = 'A'
+	}else{
+		message = 'Input Valid Score '
+	}
+	return message;
+}
